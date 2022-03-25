f6cfae91aa5853beb00d8b979d07caf0598f19b9
axa000 <raymond874100@gmail.com>
2022-03-24 19:12:56 -0700

    coding

--- main.py
+++ main.py
@@ -3,4 +3,25 @@ class Card(object):
         self.value = value
         self.suit = suit
         self.name = name
+    def __repr__(self):
+        return str(srlf.name) + ' of ' + self.suit
     
+class StandardDeck(list):
+    def __init__(self):
+        suits = ['Hearts', 'Spades', 'Diamonds', 'Clubs']
+        values = {'Two':2,
+                  'Three':3,
+                  'Four':4,
+                  'Five':5,
+                  'Six':6,
+                  'Seven':7,
+                  'Eight':8,
+                  'Nine':9,
+                  'Ten':10,
+                  'Jack':11,
+                  'Queen':12,
+                  'King':13,
+                  'Ace':14}
+        for name in values:
+            for suit in suits:
+                self.append(Card(name, values[name], suit))
